03c55c61f65ca71183ee201ef9ed37268d6e90e2
Caio de Alcantara Santos <caioalcantarasantos3@gmail.com>
2022-04-02 11:24:17 -0300

    pre-validation

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-const firstName = document.querySelector("#firstName").value;
-const lastName = document.querySelector("#lastName").value;
-const emailAdress = document.querySelector("#emailAdress").value;
-const password = document.querySelector("#password").value;
-const button = document.querySelector("#btn");
-
 function validateEmail() {
   let regex = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  let emailAdress = document.querySelector("#emailAdress").value;
 
   if (emailAdress.match(regex)) {
     return true;
@@ -15,7 +10,8 @@ function validateEmail() {
 }
 
 function validatePassword() {
-  let regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})";
+  let regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"; // Minimum eight characters, at least one uppercase letter, one lowercase letter and one number:
+  let password = document.querySelector("#password").value;
 
   if (password.match(regex)) {
     return true;
@@ -28,11 +24,26 @@ function stringContainsNumber(_string) {
   return /\d/.test(_string);
 }
 
+const button = document.querySelector("#btn");
+
 button.addEventListener("click", function (event) {
   event.preventDefault();
 
-  console.log(firstName);
-  console.log(lastName);
-  console.log(emailAdress);
-  console.log(password);
+  let firstName = document.querySelector("#firstName").value;
+  let lastName = document.querySelector("#lastName").value;
+
+  if (
+    stringContainsNumber(firstName) === true ||
+    stringContainsNumber(lastName) === true
+  ) {
+    alert("oi");
+  }
+
+  if (validateEmail() === false) {
+    alert("oi");
+  }
+
+  if (validatePassword() === false) {
+    alert("oi");
+  }
 });
